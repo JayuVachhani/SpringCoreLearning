34d61383b6c7d1a18a42340103b8f3577eb93882
jay <jay.160410116139@gmail.com>
2020-10-06 11:36:12 +0530

    stereotype annotation (no longer use of xml)

---
+++ src/main/java/com/springcore/stereotyannotation08/Student.java
@@ -0,0 +1,31 @@
+package com.springcore.stereotyannotation08;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+//	to enter value
+	@Value("Jay")
+	private String name;
+	@Value("Mumbai")
+	private String city;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", city=" + city + "]";
+	}
+	
+
+}

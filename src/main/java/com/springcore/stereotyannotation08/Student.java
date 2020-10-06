8dfabaded04b9b05e54ac82337cfafe04ff4e76a
jay <jay.160410116139@gmail.com>
2020-10-06 11:38:10 +0530

    stereotype annotation (no longer use of xml)

--- src/main/java/com/springcore/stereotyannotation08/Student.java
+++ src/main/java/com/springcore/stereotyannotation08/Student.java
@@ -3,6 +3,7 @@ package com.springcore.stereotyannotation08;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+//we can give any name of onject by passing object name @Component("object name")
 @Component
 public class Student {
 //	to enter value
